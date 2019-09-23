b440060781463032746aec318527c9999bd201b7
zlmi <15910344021@163.com>
2019-09-23 10:20:55 +0800

    css 单独分离打包

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 //引入webpack
-const  webpack = require("webpack")
+const webpack = require("webpack");
+
+// 非javascript代码的静态文件单独打包
+const ExtractPlugin = require("extract-text-webpack-plugin");
 
 // js基本路径
-const path = require("path")
+const path = require("path");
 
-const HTMLPlugin = require("html-webpack-plugin")
+const HTMLPlugin = require("html-webpack-plugin");
 
-const isDev = process.env.NODE_ENV === "development"
+const isDev = process.env.NODE_ENV === "development";
 
 // 编译后的代码都在bundle.js中
 let config = {
@@ -15,7 +18,7 @@ let config = {
     // entry 入口
     entry: path.join(__dirname, "src/index.js"),
     output: {
-        filename: "bundle.js",
+        filename: "bundle.[hash:8].js",
         path: path.join(__dirname, "dist")
     },
     module: {
@@ -33,32 +36,17 @@ let config = {
                 // loader 为webpack处理 .vue 的文件类型
                 loader: "babel-loader"
             },
-            {
-                // test 检测文件类型
-                test: /\.css$/,
-                // 把css写入到js
-                use: [
-                    "style-loader",
-                    "css-loader"
-                ]
-            },
-            {
-                test:/\.styl/,
-                use: [
-                    // 各自处理各自范围的代码
-                    // stylus-loader(处理style成css，扔给css-loader) -> css-loader -> style-loader
-                    "style-loader",
-                    "css-loader",
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    // 写起来方便
-                    "stylus-loader"
-                ]
-            },
+            // 未使用，只用了stylus
+            // {
+            //     // test 检测文件类型
+            //     test: /\.css$/,
+            //     // 把css写入到js
+            //     use: [
+            //         "style-loader",
+            //         "css-loader"
+            //     ]
+            // },
+
             {
                 test: /\.(gif|jpg|jpeg|png|svg)/,
                 use: [{
@@ -74,21 +62,38 @@ let config = {
             }
         ]
     },
-    plugins:[
+    plugins: [
         // 根据开发环境打包
         new webpack.DefinePlugin({
             "process.env": {
                 // '"字符串"'
-                NODE_ENV: isDev ? '"development"':'"production"'
+                NODE_ENV: isDev ? '"development"' : '"production"'
             }
         }),
         new HTMLPlugin()
     ]
-}
+};
 
-if(isDev) {
+if (isDev) {
+    config.module.push({
+        test: /\.styl/,
+        use: [
+            // 各自处理各自范围的代码
+            // stylus-loader(处理style成css，扔给css-loader) -> css-loader -> style-loader
+            "style-loader",
+            "css-loader",
+            {
+                loader: "postcss-loader",
+                options: {
+                    sourceMap: true
+                }
+            },
+            // 写起来方便
+            "stylus-loader"
+        ]
+    });
     // 代码映射
-    config.devtool = "#cheatp-module-eval-source-map"
+    config.devtool = "#cheatp-module-eval-source-map";
     config.devServer = {
         port: 8000,
         // 使用ip访问
@@ -98,16 +103,43 @@ if(isDev) {
             error: true
         },
         // 自动打开浏览器
-        open:true,
+        open: true,
         // historyApiFallback
         // 热更新，页面数据不会丢掉
         hot: true
 
-    }
+    };
     config.plugins.push(
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin()
     )
+} else {
+    config.output.filename = '[name].[chunkhash:8].js';
+    // 开发环境静态样式文件单独打包
+    config.module.rules.push({
+        test:/\.styl/,
+        use: ExtractPlugin.extract({
+            fallback: 'style-loader',
+            use:[
+                // 各自处理各自范围的代码
+                // stylus-loader(处理style成css，扔给css-loader) -> css-loader -> style-loader
+                "css-loader",
+                {
+                    loader: "postcss-loader",
+                    options: {
+                        sourceMap: true
+                    }
+                },
+                // 写起来方便
+                "stylus-loader"
+            ]
+        })
+
+    });
+
+    config.plugins.push(
+        new ExtractPlugin('styles.[contentHash:8].css')
+    )
 }
 
-module.exports = config
+module.exports = config;
