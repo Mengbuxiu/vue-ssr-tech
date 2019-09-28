01c7e86b75036af518fcd44e4c3d18f0a66a8aaf
zlmi <15910344021@163.com>
2019-09-28 14:13:11 +0800

    demo练习配置2

--- build/webpack.config.practice.js
+++ build/webpack.config.practice.js
@@ -6,35 +6,46 @@ const HTMLPlugin = require("html-webpack-plugin");
 const baseConfig = require("./webpack.config.base");
 const merge = require("webpack-merge");
 const defaultPlugins = [
-    // 根据开发环境打包
-    new webpack.DefinePlugin({
-        "process.env": {
-            // '"字符串"'
-            NODE_ENV: '"development"'
-        }
-    }),
-    new HTMLPlugin()
+  // 根据开发环境打包
+  new webpack.DefinePlugin({
+    "process.env": {
+      // '"字符串"'
+      NODE_ENV: '"development"'
+    }
+  }),
+  new HTMLPlugin({
+    template: path.join(__dirname, 'template.html')
+  })
 ]
 
 const devServer = {
-    port: 8080,
-    // 使用ip访问
-    host: "0.0.0.0",
-    // 将错误显示到网页上
-    overlay: {
-        error: true
-    },
-    // 自动打开浏览器
-    open: true,
-    // historyApiFallback
-    // 热更新，页面数据不会丢掉
-    hot: true
+  port: 8080,
+  // 使用ip访问
+  host: "0.0.0.0",
+  // 将错误显示到网页上
+  overlay: {
+    error: true
+  },
+  // 自动打开浏览器
+  open: true,
+  // historyApiFallback
+  /*historyApiFallback: {
+      index: '/dist/index.html'
+  },*/
+  // 热更新，页面数据不会丢掉
+  hot: true
 
 };
+
 let config;
+
 config = merge(baseConfig, {
   devServer,
-  entry: path.join(__dirname,'../practice/index.js'),
+  entry: path.join(__dirname, '../practice/index.js'),
+  output: {
+    filename: "bundle.[hash:8].js",
+    path: path.join(__dirname, "../dist_practice")
+  },
   // 代码映射
   devtool: "#cheatp-module-eval-source-map",
   resolve: {
@@ -68,5 +79,4 @@ config = merge(baseConfig, {
     new webpack.NoEmitOnErrorsPlugin()
   ])
 });
-
 module.exports = config;
