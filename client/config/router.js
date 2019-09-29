e221373e0d7dba141d54856ae1e9e9b67464ca14
zlmi <15910344021@163.com>
2019-09-29 18:03:29 +0800

    vue-router初始化

---
+++ client/config/router.js
@@ -0,0 +1,8 @@
+import Router from 'vue-router'
+import routes from "./routes";
+// 直接new在全局都是同一个router，用函数的方式每次的router不同
+export default () => {
+  return new Router({
+    routes
+  })
+}
---
+++ client/config/routes.js
@@ -0,0 +1,13 @@
+import Todo from '../view/todo/todo.vue'
+import Login from '../view/login/login.vue'
+
+export default [
+  {
+    path: 'app',
+    components: Todo
+  },
+  {
+    path: 'login',
+    components: Login
+  }
+]
