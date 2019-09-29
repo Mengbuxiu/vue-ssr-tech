48b0865b0a44a1fa4e806da5885f62739c94e078
zlmi <15910344021@163.com>
2019-09-29 17:43:04 +0800

    vue 多选框

--- practice/computed/index.js
+++ practice/computed/index.js
@@ -1,5 +1,18 @@
 import Vue from 'vue'
 
 new Vue({
-  el: '#root'
+  el: '#root',
+  template: `
+      <div>
+        <input type="checkbox" :value="1" v-model="arr"/>
+        <input type="checkbox" :value="2" v-model="arr"/>
+        <input type="checkbox" :value="3" v-model="arr"/>
+      </div>
+    `,
+  data: {
+    arr: [1, 2, 3]
+  },
+  computed: {
+    // 缓存方法
+  }
 })
