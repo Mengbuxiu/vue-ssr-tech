bfb9c3a227e607e6801e4efeb022b76fd79652ad
zlmi <15910344021@163.com>
2019-10-09 17:05:23 +0800

    lint fix

--- client/config/router.js
+++ client/config/router.js
@@ -1,5 +1,5 @@
 import Router from 'vue-router'
-import routes from "./routes";
+import routes from './routes'
 // 直接new在全局都是同一个router，用函数的方式每次的router不同
 export default () => {
   return new Router({
